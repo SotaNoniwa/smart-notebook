9a2e824f514c7e90383fdd13554463ead3408fc8
SotaNoniwa <surfersou@gmail.com>
2023-12-05 09:32:37 +0900

    give title to button element

--- src/components/TipTapMenuBar.tsx
+++ src/components/TipTapMenuBar.tsx
@@ -27,6 +27,7 @@ const TipTapMenuBar = ({ editor }: Props) => {
   return (
     <div className="flex flex-wrap gap-2">
       <button
+        title="bold"
         onClick={() => editor.chain().focus().toggleBold().run()}
         disabled={!editor.can().chain().focus().toggleBold().run()}
         className={editor.isActive("bold") ? "is-active" : ""}
@@ -34,6 +35,7 @@ const TipTapMenuBar = ({ editor }: Props) => {
         <Bold className="w-6 h-6" />
       </button>
       <button
+        title="italic"
         onClick={() => editor.chain().focus().toggleItalic().run()}
         disabled={!editor.can().chain().focus().toggleItalic().run()}
         className={editor.isActive("italic") ? "is-active" : ""}
@@ -41,6 +43,7 @@ const TipTapMenuBar = ({ editor }: Props) => {
         <Italic className="w-6 h-6" />
       </button>
       <button
+        title="strike"
         onClick={() => editor.chain().focus().toggleStrike().run()}
         disabled={!editor.can().chain().focus().toggleStrike().run()}
         className={editor.isActive("strike") ? "is-active" : ""}
@@ -48,6 +51,7 @@ const TipTapMenuBar = ({ editor }: Props) => {
         <Strikethrough className="w-6 h-6" />
       </button>
       <button
+        title="code"
         onClick={() => editor.chain().focus().toggleCode().run()}
         disabled={!editor.can().chain().focus().toggleCode().run()}
         className={editor.isActive("code") ? "is-active" : ""}
@@ -55,72 +59,84 @@ const TipTapMenuBar = ({ editor }: Props) => {
         <Code className="w-6 h-6" />
       </button>
       <button
+        title="heading-1"
         onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
         className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
       >
         <Heading1 className="w-6 h-6" />
       </button>
       <button
+        title="heading-2"
         onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
         className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
       >
         <Heading2 className="w-6 h-6" />
       </button>
       <button
+        title="heading-3"
         onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
         className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
       >
         <Heading3 className="w-6 h-6" />
       </button>
       <button
+        title="heading-4"
         onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
         className={editor.isActive("heading", { level: 4 }) ? "is-active" : ""}
       >
         <Heading4 className="w-6 h-6" />
       </button>
       <button
+        title="heading-5"
         onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
         className={editor.isActive("heading", { level: 5 }) ? "is-active" : ""}
       >
         <Heading5 className="w-6 h-6" />
       </button>
       <button
+        title="heading-6"
         onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
         className={editor.isActive("heading", { level: 6 }) ? "is-active" : ""}
       >
         <Heading6 className="w-6 h-6" />
       </button>
       <button
+        title="bullet-list"
         onClick={() => editor.chain().focus().toggleBulletList().run()}
         className={editor.isActive("bulletList") ? "is-active" : ""}
       >
         <List className="w-6 h-6" />
       </button>
       <button
+        title="ordered-list"
         onClick={() => editor.chain().focus().toggleOrderedList().run()}
         className={editor.isActive("orderedList") ? "is-active" : ""}
       >
         <ListOrdered className="w-6 h-6" />
       </button>
       <button
+        title="code-block"
         onClick={() => editor.chain().focus().toggleCodeBlock().run()}
         className={editor.isActive("codeBlock") ? "is-active" : ""}
       >
         <CodepenIcon className="w-6 h-6" />
       </button>
       <button
+        title="block-quote"
         onClick={() => editor.chain().focus().toggleBlockquote().run()}
         className={editor.isActive("blockquote") ? "is-active" : ""}
       >
         <Quote className="w-6 h-6" />
       </button>
       <button
+        title="undo"
         onClick={() => editor.chain().focus().undo().run()}
         disabled={!editor.can().chain().focus().undo().run()}
       >
         <Undo className="w-6 h-6" />
       </button>
       <button
+        title="redo"
         onClick={() => editor.chain().focus().redo().run()}
         disabled={!editor.can().chain().focus().redo().run()}
       >
